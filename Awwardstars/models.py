9ea8afc7929668f8e03e743f36cc2e8795f6d5f7
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-10-26 20:28:15 +0300

    Created model Projects

--- Awwardstars/models.py
+++ Awwardstars/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
+class Projects(models.Model):
+    title = models.CharField(max_length=30)
+    landing_page = CloudinaryField('image')
+    description = models.CharField(max_length=150)
+    link = models.CharField(max_length=100)
+
+    def save_profile(self):
+         self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def __str__(self):
+        return self.username
--- awwards/settings.py
+++ awwards/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Awwardstars.app.AwwardstarConfig',
+    'Awwardstars.apps.AwwardstarsConfig',
     'cloudinary',
     'django_registration',
     'bootstrap3',
