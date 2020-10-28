d1c79777f7cb9991c4397d13367449e569b226e1
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-10-28 14:37:27 +0300

    Edit Profile view

--- Awwardstars/urls.py
+++ Awwardstars/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url('',views.index, name='index'),
+    url('profile/edit/',views.edit_profile,name='edit-profile')
 ]
--- Awwardstars/views.py
+++ Awwardstars/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render,redirect
-
+from django.contrib.auth.decorators import login_required
+from .models import Projects,Profile
+from django.contrib.auth.models import User
+from .forms import EditProfileForm
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
+@login_required(login_url='/accounts/login/')
+def edit_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+        return redirect('profile')
+
+    else:
+        form = EditProfileForm()
+    return render(request, 'edit_profile.html', {"form": form})
