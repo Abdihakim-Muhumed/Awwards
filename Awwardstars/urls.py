8fbf7b5b191a54b9d860a27cd8dff3dcab53e02a
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-10-28 15:01:52 +0300

    Profile view

--- Awwardstars/urls.py
+++ Awwardstars/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 urlpatterns = [
-    url('',views.index, name='index'),
-    url('profile/edit/',views.edit_profile,name='edit-profile')
+    path('',views.index, name='index'),
+    path('edit/',views.edit_profile,name='edit-profile'),
+    path('my_profile/',views.profile,name='profile'),
 ]
--- Awwardstars/views.py
+++ Awwardstars/views.py
@@ -6,6 +6,17 @@ from .forms import EditProfileForm
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    try:
+        profile = Profile.objects.filter(user = current_user).first()
+    except:
+        return redirect('edit-profile')
+    projects = Projects.objects.filter(user = current_user).all()
+    total_projects = projects.count()
+    return render(request,'profile.html',{"profile":profile,"projects":projects,"current_user":current_user,"total_projects":total_projects,})
+
 
 @login_required(login_url='/accounts/login/')
 def edit_profile(request):
