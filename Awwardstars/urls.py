a632318b581905e15f5e94d08a5f20f434f2eabd
“Abdihakim <abdihakim.0017@gmail.com.comgit config --global user.name abdihakim17git config --global user.email abdihakim.0017@gmail.com.com>
2020-10-28 14:32:57 +0300

    Edit Profile form creation

--- Awwardstars/urls.py
+++ Awwardstars/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 from . import views
-from django_re
 urlpatterns = [
     url('',views.index, name='index'),
 ]
--- Awwardstars/views.py
+++ Awwardstars/views.py
@@ -4,17 +4,3 @@ from django.shortcuts import render,redirect
 def index(request):
     return render(request,'index.html')
 
-@login_required(login_url='/accounts/login/')
-def edit_profile(request):
-    current_user = request.user
-    if request.method == 'POST':
-        form = EditProfileForm(request.POST, request.FILES)
-        if form.is_valid():
-            profile = form.save(commit=False)
-            profile.user = current_user
-            profile.save()
-        return redirect('profile')
-
-    else:
-        form = EditProfileForm()
-    return render(request, 'edit_profile.html', {"form": form})
